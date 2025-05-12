1f93578bd03453ef0b7e54098da9ae91d99083f3
RISHYAVANDHAN <rishyavandhan.v@gmail.com>
2025-05-12 12:47:26 +0200

    Updated build.sh
    Signed-off-by: RISHYAVANDHAN <rishyavandhan.v@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-# Create build directory if it doesn't exist
+git submodule update --init --recursive
 mkdir -p build
 cd build
-
-# Configure CMake and build
 cmake ..
 cmake --build .
 
-echo "Build complete. Executable is in build/peridynamics_2d"
+echo "Build complete. Executable is ready"
